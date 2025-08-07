7512b1af50897b7389605c9df975a407a3207397
IsisMarieli <Isismarieli1@gmail.com>
2025-08-06 22:50:38 -0300

    first commit

---
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-certificado',
+  imports: [],
+  templateUrl: './item-certificado.component.html',
+  styleUrl: './item-certificado.component.css'
+})
+export class ItemCertificadoComponent {
+
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gerador-certificado';
+  exbibenavbar: boolean = false;
+}

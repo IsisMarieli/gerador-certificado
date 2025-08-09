fab80e287b88b3b8606d671cef50d1d83ef261f6
IsisMarieli <Isismarieli1@gmail.com>
2025-08-09 14:15:37 -0300

    commit item-certificado

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [],
+  imports: [SecondaryButtonComponent],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })

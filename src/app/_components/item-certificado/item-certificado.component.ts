4c1a9ac3b08d99c1c4f898fa526b9c2c8e289c2c
IsisMarieli <Isismarieli1@gmail.com>
2025-09-03 23:11:47 -0300

    rotas

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent,],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css'
 })
 export class ItemCertificadoComponent {
+  id: string = '6'
 
+  // outra forma de navegar nas rotas(via typescript)
+    constructor(private router:Router){ }
+
+    redirecionaCertificado(){
+      // this.router.navigate(['/certificados' + this.id]); // -> /certificados/2 , em alguns casos
+      this.router.navigate(['/certificados',2]); // -> /certificados/2 , dinâmico
+      // this.router.navigateByUrl("/certificados/2"); // -> /certificados/2 , fixo
+    }
 }
